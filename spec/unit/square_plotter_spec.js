69d969d29f062411fd2383313e9732704d263d50
Andrew Bruce <andrew@camelpunch.com>
2012-06-08 01:02:28 -0700

    limit of one square per menu click

--- spec/unit/square_plotter_spec.js
+++ spec/unit/square_plotter_spec.js
@@ -4,30 +4,62 @@
   "use strict";
 
   describe("unit: square plotter", function () {
-    it("draws a square using mouse events", function () {
-      var expectedDimensions = WHITEBOARD.createDimensions(4, 8, 20, 20),
-        ns = "http://www.w3.org/2000/svg",
-        canvas = jQuery(document.createElementNS(ns, 'svg')),
-        plotter = WHITEBOARD.createSquarePlotter(canvas[0], document),
-        startPosition,
-        endPosition,
-        rect;
+    describe("starting", function () {
+      it("draws a square using mouse events", function () {
+        var expectedDimensions = WHITEBOARD.createDimensions(4, 8, 20, 20),
+          ns = "http://www.w3.org/2000/svg",
+          canvas = jQuery(document.createElementNS(ns, 'svg')),
+          plotter = WHITEBOARD.createSquarePlotter(canvas[0], document),
+          startPosition,
+          endPosition,
+          rect;
 
-      startPosition = { offsetX: 4, offsetY: 8 };
-      endPosition = { offsetX: 14, offsetY: 28 };
+        startPosition = { offsetX: 4, offsetY: 8 };
+        endPosition = { offsetX: 14, offsetY: 28 };
 
-      plotter.start();
+        plotter.start();
 
-      canvas.trigger(jQuery.Event('mousedown', startPosition));
+        canvas.trigger(jQuery.Event('mousedown', startPosition));
 
-      rect = canvas.find('rect');
-      expect(rect).not.toExist();
+        rect = canvas.find('rect');
+        expect(rect).not.toExist();
 
-      canvas.trigger(jQuery.Event('mouseup', endPosition));
+        canvas.trigger(jQuery.Event('mouseup', endPosition));
 
-      rect = canvas.find('rect');
-      expect(rect).toExist();
-      expect(rect).toHaveDimensions(expectedDimensions);
+        rect = canvas.find('rect');
+        expect(rect).toExist();
+        expect(rect).toHaveDimensions(expectedDimensions);
+      });
+    });
+
+    describe("stopping", function () {
+      it("clears any previously drawn square", function () {
+        var expectedDimensions = WHITEBOARD.createDimensions(4, 8, 20, 20),
+          ns = "http://www.w3.org/2000/svg",
+          canvas = jQuery(document.createElementNS(ns, 'svg')),
+          plotter = WHITEBOARD.createSquarePlotter(canvas[0], document),
+          startPosition,
+          endPosition,
+          rect;
+
+        startPosition = { offsetX: 4, offsetY: 8 };
+        endPosition = { offsetX: 14, offsetY: 28 };
+
+        plotter.start();
+
+        canvas.trigger(jQuery.Event('mousedown', startPosition));
+        canvas.trigger(jQuery.Event('mouseup', endPosition));
+
+        rect = canvas.find('rect');
+        expect(rect).toExist();
+
+        canvas.trigger(jQuery.Event('mousedown', startPosition));
+        rect = canvas.find('rect');
+        expect(rect.length).toBe(1);
+        canvas.trigger(jQuery.Event('mouseup', endPosition));
+        rect = canvas.find('rect');
+        expect(rect.length).toBe(1);
+      });
     });
   });
 }());
--- src/square_plotter.js
+++ src/square_plotter.js
@@ -11,29 +11,32 @@
     self = {
       start: function () {
         var rect, x, y,
-          eventWidth = function (e) {
-            return e.offsetX - x;
-          },
-          eventHeight = function (e) {
-            return e.offsetY - y;
-          };
+          eventWidth = function (e) { return e.offsetX - x; },
+          eventHeight = function (e) { return e.offsetY - y; };
 
         rect = context.createElementNS(ns, 'rect');
 
         jQuery(el)
-          .mousedown(function (e) {
+          .on('mousedown', function (e) {
+            if (!rect) {
+              jQuery(el)
+                .off('mousedown')
+                .off('mouseup');
+              return;
+            }
             x = e.offsetX;
             y = e.offsetY;
             rect.setAttribute('x', e.offsetX);
             rect.setAttribute('y', e.offsetY);
           })
-          .mouseup(function (e) {
+          .on('mouseup', function (e) {
             var side = Math.max(eventWidth(e), eventHeight(e));
             rect.setAttribute('width', side);
             rect.setAttribute('height', side);
             el.appendChild(rect);
+            rect = undefined;
           });
-      }
+      },
     };
     return self;
   };
